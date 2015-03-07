e2b31b17cd719510e20763e103454377e0b19e6e
RaulButuc <raulbutuc@gmail.com>
2015-03-07 21:48:40 +0000

    Updated the StringConverter class

--- StringCoverter/StringConverter.h
+++ StringCoverter/StringConverter.h
@@ -1,9 +1,9 @@
 /**
  *  StringConverter.h
- *  Purpose: Enable easy conversion from string to wstring and LPCWSTR formats.
+ *  Purpose: Enable easy conversion from string to lpConstWideStr and other string types.
  *
  *  @author Raul Butuc
- *  @version 1.0.0 06/03/2015
+ *  @version 1.0.1 07/03/2015
  */
 
 #pragma once
@@ -19,25 +19,23 @@ namespace StringHelper {
 	template <typename _StrS, typename _StrT>
 	class StringConverter {
 
-	public:
-		StringConverter();
-		static _StrS convert(_StrT);
-		~StringConverter();
+		public:
+			static _StrS convert(_StrT);
 
 	};
 
+	/**
+	 *  Convert a string of type _StrT to a string of _StrS.
+	 *
+	 *  @param _str The string of type _StrT that is to be converted.
+	 *  @return The converted string into the desired format (assuming long pointer constant type).
+	 */
 	template <typename _StrS, typename _StrT>
-	StringConverter<_StrS, _StrT>::StringConverter() {}
-
-	template <typename _StrS, typename _StrT>
-	_StrS StringConverter<_StrS, _StrT>::convert(_StrT str) {
-		wchar_t* wString = new wchar_t[2 * strlen(str.c_str())];
-		MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, wString, 2 * strlen(str.c_str()));
+	_StrS StringConverter<_StrS, _StrT>::convert(_StrT _str) {
+		wchar_t* wString = new wchar_t[2 * strlen(_str.c_str())];
+		MultiByteToWideChar(CP_ACP, 0, _str.c_str(), -1, wString, 2 * strlen(_str.c_str()));
 
 		return wString;
 	}
-
-	template <typename _StrS, typename _StrT>
-	StringConverter<_StrS, _StrT>::~StringConverter() {}
 
 }
