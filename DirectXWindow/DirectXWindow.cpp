0e0d117ca795e275e1df0bf27720bd1e2fac9a64
RaulButuc <raulbutuc@gmail.com>
2015-03-07 21:47:58 +0000

    Updated the DirectXWindow class

--- DirectXWindow/DirectXWindow.cpp
+++ DirectXWindow/DirectXWindow.cpp
@@ -3,30 +3,55 @@
  *  Purpose: Build a DirectX managed window.
  *
  *  @author Raul Butuc
- *  @version 1.0.0 06/03/2015
+ *  @version 1.0.1 07/03/2015
  */
 
 #include "DirectXWindow.h"
 
 namespace DirectXApplication {
-	
-	DirectXWindow::DirectXWindow(HINSTANCE hInstance) : Window(hInstance) {}
 
+	/**
+	 *  Call the base class constructor with the same parameters.
+	 *
+	 *  @param hInstance The current instance handler.
+	 *  @param ClientWidth The width of the client.
+	 *  @param ClientHeight The height of the client.
+	 *  @param AppTitle The title for the window.
+	 */
 	DirectXWindow::DirectXWindow(HINSTANCE hInstance, UINT ClientWidth, UINT ClientHeight, string AppTitle) :
 		Window(hInstance, ClientWidth, ClientHeight, AppTitle) {}
 
+	/**
+	 *  Initialise the DirectX window.
+	 *
+	 *  @return The state after trying to initialise the window.
+	 */
 	bool DirectXWindow::Init() {
 		return Window::Init();
 	}
 
+	/**
+	 *  Update the DirectX window.
+	 *
+	 *  @param dt The period of time after which the window should update.
+	 */
 	void DirectXWindow::Update(float dt) {
-		// @TODO: Create the Update function, given delta t.
+		// @TODO: Write the Update function, given delta t.
 	}
 
+	/**
+	 *  Render the DirectX window.
+	 *
+	 *  @param dt The period of time after which the window should render.
+	 */
 	void DirectXWindow::Render(float dt) {
-		// @TODO: Create the Render function, given delta t.
+		m_pImmediateContext->ClearRenderTargetView(m_pRenderTargetView, DirectX::Colors::CornflowerBlue);
+		m_pSwapChain->Present(0, 0);
 	}
 
+	/**
+	 *  Default destructor.
+	 */
 	DirectXWindow::~DirectXWindow() {}
 
 }
--- DirectXWindow/DirectXWindow.h
+++ DirectXWindow/DirectXWindow.h
@@ -3,7 +3,7 @@
  *  Purpose: Build a DirectX managed window.
  *
  *  @author Raul Butuc
- *  @version 1.0.0 06/03/2015
+ *  @version 1.0.1 07/03/2015
  */
 
 #pragma once
@@ -14,13 +14,12 @@ namespace DirectXApplication {
 
 	class DirectXWindow : public Window {
 
-	public:
-		DirectXWindow(HINSTANCE);
-		DirectXWindow(HINSTANCE, UINT, UINT, string);
-		bool Init() override;
-		void Update(float) override;
-		void Render(float) override;
-		~DirectXWindow();
+		public:
+			DirectXWindow(HINSTANCE, UINT, UINT, string);
+			bool Init() override;
+			void Update(float) override;
+			void Render(float) override;
+			~DirectXWindow();
 
 	};
 
