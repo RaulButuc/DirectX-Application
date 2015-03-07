efbf1fcf69b927b1c66a9b9649596316e7adbea4
RaulButuc <raulbutuc@gmail.com>
2015-03-07 22:17:44 +0000

    Added DirectX11 functionality

--- Window/Window.cpp
+++ Window/Window.cpp
@@ -1,27 +1,26 @@
-/**
+﻿/**
  *  Window.cpp
  *  Purpose: Build a standard WIN32 window.
  *
  *  @author Raul Butuc
- *  @version 1.0.0 06/03/2015
+ *  @version 1.0.1 07/03/2015
  */
 
 #include "Window.h"
 
 namespace DirectXApplication {
 
+	// Used to forward messages to user defined procedure fuction.
 	Window* g_pApp = nullptr;
 
-	Window::Window(HINSTANCE hInstance) {
-		m_hAppInstance = hInstance;
-		m_hAppWnd = NULL;
-		m_ClientWidth = 800;
-		m_ClientHeight = 600;
-		m_AppTitle = "Title";
-		m_WndStyle = WS_OVERLAPPEDWINDOW;
-		g_pApp = this;
-	}
-
+	/**
+	 *  Constructor for a window.
+	 *
+	 *  @param hInstance The current instance handler.
+	 *  @param ClientWidth The width of the client.
+	 *  @param ClientHeight The height of the client.
+	 *  @param AppTitle The title for the window.
+	 */
 	Window::Window(HINSTANCE hInstance, UINT ClientWidth, UINT ClientHeight, string AppTitle) {
 		m_hAppInstance = hInstance;
 		m_hAppWnd = NULL;
@@ -30,8 +29,18 @@ namespace DirectXApplication {
 		m_AppTitle = AppTitle;
 		m_WndStyle = WS_OVERLAPPEDWINDOW;
 		g_pApp = this;
+
+		m_pDevice = nullptr;
+		m_pImmediateContext = nullptr;
+		m_pRenderTargetView = nullptr;
+		m_pSwapChain = nullptr;
 	}
 
+	/**
+	 *  Run the window and fetch the window message as long as not being told otherwise.
+	 *  
+	 *  @return An integer representing the state 
+	 */
 	int Window::Run() {
 		MSG msg = { 0 };
 
@@ -49,10 +58,34 @@ namespace DirectXApplication {
 		return static_cast<int>(msg.wParam);
 	}
 
+	/**
+	 *  Framework method that initialises the window.
+	 *
+	 *  @return A boolean representing the success/failure state received after executing the init methods.
+	 */
 	bool Window::Init() {
-		return InitWindow();
+		// Initialise window.
+		if (!InitWindow()) {
+			return false;
+		}
+
+		// Initialise DirectX window.
+		if (!InitDirect3D()) {
+			return false;
+		}
+
+		return true;
 	}
 
+	/**
+	 *  Windows message procedure method.
+	 *
+	 *  @param hwnd Current window handler.
+	 *  @param msg The windows message.
+	 *  @param wParam A message parameter.
+	 *  @param lParam A message parameter.
+	 *  @return A long pointer, either nullptr or the result obtained from the default window procedure.
+	 */
 	LRESULT Window::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 		switch (msg) {
 		case WM_DESTROY:
@@ -64,6 +97,15 @@ namespace DirectXApplication {
 		}
 	}
 
+	/**
+	 *  Windows callback message procedure method.
+	 *
+	 *  @param hwnd Current window handler.
+     *  @param msg The windows message.
+	 *  @param wParam A message parameter.
+	 *  @param lParam A message parameter.
+	 *  @return A long pointer, either nullptr or the result obtained from the default window procedure.
+	 */
 	LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 		if (g_pApp) {
 			return g_pApp->MsgProc(hwnd, msg, wParam, lParam);
@@ -72,8 +114,26 @@ namespace DirectXApplication {
 		return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
 
-	Window::~Window() {}
+	/**
+	 *  Destructor. Deallocate all the memory used.
+	 */
+	Window::~Window() {
+		if (m_pImmediateContext) {
+			m_pImmediateContext->ClearState();
+		}
 
+		MemoryManagement::SafeRelease(m_pRenderTargetView);
+		MemoryManagement::SafeRelease(m_pSwapChain);
+		MemoryManagement::SafeRelease(m_pImmediateContext);
+		MemoryManagement::SafeRelease(m_pDevice);
+	}
+
+	/**
+	 *  First part of the main framework method that initialises the application window.
+	 *  Deals with the main window display part.
+	 *
+	 *  @return A boolean representing the success/failure state received after executing the init methods.
+	 */
 	bool Window::InitWindow() {
 		WNDCLASSEX wcex;
 		ZeroMemory(&wcex, sizeof(WNDCLASSEX));
@@ -118,6 +178,86 @@ namespace DirectXApplication {
 		}
 
 		ShowWindow(m_hAppWnd, SW_SHOW);
+
+		return true;
+	}
+
+	/**
+	 *  Second part of the main framework method that initialises the application window.
+	 *  Deals with the DirectX display part.
+	 *
+	 *  @return A boolean representing the success/failure state received after executing the init methods.
+	 */
+	bool Window::InitDirect3D() {
+		UINT createDeviceFlags = 0;
+
+		#ifdef _DEBUG
+				createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+		#endif
+
+		D3D_DRIVER_TYPE driverTypes[] = {
+			D3D_DRIVER_TYPE_HARDWARE,
+			D3D_DRIVER_TYPE_WARP,
+			D3D_DRIVER_TYPE_REFERENCE
+		};
+		UINT numDriverTypes = ARRAYSIZE(driverTypes);
+
+		D3D_FEATURE_LEVEL featureLevels[] = {
+			D3D_FEATURE_LEVEL_11_0,
+			D3D_FEATURE_LEVEL_10_1,
+			D3D_FEATURE_LEVEL_10_0,
+			D3D_FEATURE_LEVEL_9_3
+		};
+		UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
+		DXGI_SWAP_CHAIN_DESC swapDesc;
+		ZeroMemory(&swapDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
+		swapDesc.BufferCount = 1;
+		swapDesc.BufferDesc.Width = m_ClientWidth;
+		swapDesc.BufferDesc.Height = m_ClientHeight;
+		swapDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		swapDesc.BufferDesc.RefreshRate.Numerator = 60;
+		swapDesc.BufferDesc.RefreshRate.Denominator = 1;
+		swapDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		swapDesc.OutputWindow = m_hAppWnd;
+		swapDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+		swapDesc.Windowed = true;
+		swapDesc.SampleDesc.Count = 1;
+		swapDesc.SampleDesc.Quality = 0;
+		swapDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+		HRESULT result;
+		for (int i = 0; i < numDriverTypes; ++i) {
+			result = D3D11CreateDeviceAndSwapChain(NULL, driverTypes[i], NULL, createDeviceFlags,
+				featureLevels, numFeatureLevels, D3D11_SDK_VERSION, &swapDesc, &m_pSwapChain, &m_pDevice,
+				&m_FeatureLevel, &m_pImmediateContext);
+
+			if (SUCCEEDED(result)) {
+				m_DriverType = driverTypes[i];
+				break;
+			}
+		}
+
+		if (FAILED(result)) {
+			OutputDebugString(StringConverter<LPCWSTR, string>::convert("Failed to create device and swap chain."));
+			
+			return false;
+		}
+
+		ID3D11Texture2D* m_pBackBufferTex = 0;
+		m_pSwapChain->GetBuffer(NULL, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&m_pBackBufferTex));
+		m_pDevice->CreateRenderTargetView(m_pBackBufferTex, nullptr, &m_pRenderTargetView);
+
+		m_pImmediateContext->OMSetRenderTargets(1, &m_pRenderTargetView, nullptr);
+
+		m_Viewport.Width = static_cast<float>(m_ClientWidth);
+		m_Viewport.Height = static_cast<float>(m_ClientHeight);
+		m_Viewport.TopLeftX = 0;
+		m_Viewport.TopLeftY = 0;
+		m_Viewport.MinDepth = 0.0f;
+		m_Viewport.MaxDepth = 1.0f;
+
+		m_pImmediateContext->RSSetViewports(1, &m_Viewport);
 
 		return true;
 	}
--- Window/Window.h
+++ Window/Window.h
@@ -3,7 +3,7 @@
  *  Purpose: Build a standard WIN32 window.
  *
  *  @author Raul Butuc
- *  @version 1.0.0 06/03/2015
+ *  @version 1.0.1 07/03/2015
  */
 
 #pragma once
@@ -12,6 +12,7 @@
 #include <Windows.h>
 #include <string>
 #include "StringConverter.h"
+#include "DirectXUtilities.h"
 
 using StringHelper::StringConverter;
 
@@ -19,26 +20,38 @@ namespace DirectXApplication {
 
 	class Window {
 
-	public:
-		Window(HINSTANCE);
-		Window(HINSTANCE, UINT, UINT, string);
-		int Run();
-		virtual bool Init();
-		virtual void Update(float) = 0;
-		virtual void Render(float) = 0;
-		virtual LRESULT MsgProc(HWND, UINT, WPARAM, LPARAM);
-		virtual ~Window();
+		public:
+			Window(HINSTANCE, UINT, UINT, string);
+			int Run();
+			virtual bool Init();
+			virtual void Update(float) = 0;
+			virtual void Render(float) = 0;
+			virtual LRESULT MsgProc(HWND, UINT, WPARAM, LPARAM);
+			virtual ~Window();
 
-	protected:
-		HWND		m_hAppWnd;
-		HINSTANCE	m_hAppInstance;
-		UINT		m_ClientWidth;
-		UINT		m_ClientHeight;
-		string		m_AppTitle;
-		DWORD		m_WndStyle;
+		protected:
+			// Win32 attributes declaration.
+			HWND		m_hAppWnd;
+			HINSTANCE	m_hAppInstance;
+			UINT		m_ClientWidth;
+			UINT		m_ClientHeight;
+			string		m_AppTitle;
+			DWORD		m_WndStyle;
 
-		bool InitWindow();
+			// DirectX11 attributes declaration.
+			ID3D11Device*				m_pDevice;
+			ID3D11DeviceContext*		m_pImmediateContext;
+			IDXGISwapChain*				m_pSwapChain;
+			ID3D11RenderTargetView*		m_pRenderTargetView;
+			D3D_DRIVER_TYPE				m_DriverType;
+			D3D_FEATURE_LEVEL			m_FeatureLevel;
+			D3D11_VIEWPORT				m_Viewport;
 
+			// Win32 init fuction.
+			bool InitWindow();
+
+			// DirectX11 init function.
+			bool InitDirect3D();
 	};
 
 }
