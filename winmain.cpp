bb97c36c3b33702ee219200fc79efebe38c9b493
RaulButuc <raulbutuc@gmail.com>
2015-03-07 17:33:27 +0000

    Updated the winmain

--- winmain.cpp
+++ winmain.cpp
@@ -1,11 +1,19 @@
+/**
+ *  winmain.cpp
+ *  Purpose: Run the application and test its functionality.
+ *
+ *  @author Raul Butuc
+ *  @version 1.0.0 06/03/2015
+ */
+
 #include "DirectXWindow.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
-	DirectXApplication::DirectXWindow tApp(hInstance, 800, 600, "DirectX11 Application");
+	DirectXApplication::DirectXWindow DXApp(hInstance, 1280, 720, "DirectX11 Application");
 
-	if (!tApp.Init()) {
+	if (!DXApp.Init()) {
 		return 1;
 	}
 
-	return tApp.Run();
+	return DXApp.Run();
 }
