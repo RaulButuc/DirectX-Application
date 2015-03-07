3c014b2fe6aa3aac3092d2f5b73777e7deda4df3
RaulButuc <raulbutuc@gmail.com>
2015-03-07 17:32:25 +0000

    Updated to use generics in StringConverter

--- StringCoverter/StringConverter.h
+++ StringCoverter/StringConverter.h
@@ -1,3 +1,11 @@
+/**
+ *  StringConverter.h
+ *  Purpose: Enable easy conversion from string to wstring and LPCWSTR formats.
+ *
+ *  @author Raul Butuc
+ *  @version 1.0.0 06/03/2015
+ */
+
 #pragma once
 
 #define WIN32_LEAN_AND_MEAN
@@ -9,14 +17,28 @@ using std::wstring;
 
 namespace StringHelper {
 
+	template <typename _StrS, typename _StrT>
 	class StringConverter {
 
 	public:
 		StringConverter();
-		static wstring toWString(string);
-		static LPCWSTR toLPCWSTR(string);
+		static _StrS convert(_StrT);
 		~StringConverter();
 
 	};
+
+	template <typename _StrS, typename _StrT>
+	StringConverter<_StrS, _StrT>::StringConverter() {}
+
+	template <typename _StrS, typename _StrT>
+	_StrS StringConverter<_StrS, _StrT>::convert(_StrT str) {
+		wchar_t* wString = new wchar_t[2 * strlen(str.c_str())];
+		MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, wString, 2 * strlen(str.c_str()));
+
+		return wString;
+	}
+
+	template <typename _StrS, typename _StrT>
+	StringConverter<_StrS, _StrT>::~StringConverter() {}
 
 }
